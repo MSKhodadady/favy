889765f06fc9993caac473a9a5c2da72e1c9203c
mohammad-sadeq-khodadadei <m.s.khodadadei@gmail.com>
2024-08-18 14:27:28 +0330

    fix: home page responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,23 +12,38 @@ export default function HomePage() {
 
   return (
     <div
-      className=" pt-3 min-h-lvh overflow-hidden flex flex-col items-center"
+      className="
+        pt-3 min-h-lvh overflow-hidden flex flex-col items-center
+        md:flex-row
+      "
       style={{
         background: "linear-gradient(134.68deg, #D76161 0.56%, #A70000 100%)",
       }}
     >
       <div
-        className="rounded-2xl bg-white flex flex-col text-center py-10 items-center"
-        style={{ width: "102%" }}
+        className="
+          w-full md:w-1/2 md:p-5
+        "
       >
-        <Image src={AppLogo} alt="app logo" className="w-1/2 h-auto" />
-        <h1 className="text-5xl my-5">فِیوی</h1>
-        <h2 className="text-xl">
-          <em>علایقت رو به دوستات نشون بده!</em>
-        </h2>
+        <div
+          className="
+           rounded-2xl bg-white flex flex-col text-center py-10 items-center w-[102%] 
+          "
+        >
+          <Image src={AppLogo} alt="app logo" className="w-1/2 h-auto" />
+          <h1 className="text-5xl my-5">فِیوی</h1>
+          <h2 className="text-xl">
+            <em>علایقت رو به دوستات نشون بده!</em>
+          </h2>
+        </div>
       </div>
 
-      <div className="text-white text-lg mt-3 text-center py-5 space-y-2 p-2 w-full">
+      <div
+        className="
+          text-white text-lg mt-3 text-center py-5 gap-2 p-2 w-full
+          flex flex-col items-stretch justify-center
+          md:w-1/2 md:p-3"
+      >
         <Link href={"/sign-in"}>
           <button
             type="button"
@@ -52,7 +67,7 @@ export default function HomePage() {
             علایق دوستت رو پیدا کن!
           </button>
         ) : (
-          <div className="flex items-center rounded-full bg-white overflow-clip p-2">
+          <div className="flex items-center rounded-full bg-white p-2">
             <button
               type="button"
               className="btn btn-primary rounded-full  p-3 min-h-fit h-fit "
@@ -64,7 +79,7 @@ export default function HomePage() {
               ref={searchInputRef}
               name=""
               id=""
-              className="input flex-grow text-black ms-2 border-none active:border-none h-100%"
+              className="input flex-grow text-black ms-2 border-none active:border-none h-100% placeholder:text-sm w-full"
               placeholder="علایق دوستت رو پیدا کن!"
             />
             <button
